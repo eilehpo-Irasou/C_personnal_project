c7cd93400cff1af1dfb7a760d2cac8527fcde7bc
eilehpo-Irasou <51323756+eilehpo-Irasou@users.noreply.github.com>
2020-04-15 14:05:48 +0200

    Add files via upload

---
+++ multiplication/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// print some message about multiplication table
+
+void welcome_messsage()
+{
+	printf("Hello !\n");
+	printf("You choose your table to print.\n");
+	printf("After, you choose a limit to print 
+		all the results.\n");
+}
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
