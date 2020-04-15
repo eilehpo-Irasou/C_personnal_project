7f5cdae5afea201be3d54a7daa8009ffd6efddc4
eilehpo-Irasou <51323756+eilehpo-Irasou@users.noreply.github.com>
2020-04-15 14:54:15 +0200

    Add files via upload

--- multiplication/main.c
+++ multiplication/main.c
@@ -1,17 +1,49 @@
 #include <stdio.h>
 
-// print some message about multiplication table
+// print some messages about multiplication table
 
-void welcome_messsage()
+void welcome_message()
 {
 	printf("Hello !\n");
 	printf("You choose your table to print.\n");
-	printf("After, you choose a limit to print 
-		all the results.\n");
+	printf("After, you choose a limit to print all the results.\n");
+}
+
+void choose_your_table_message()
+{
+	printf("Choose your table :\n");
+}
+
+void choose_your_limit_message()
+{
+	printf("Choose your limit :\n");
+}
+
+
+int multiplication()
+{
+	// variables
+	int my_table;
+	int my_limit;
+	int i;
+	choose_your_table_message();
+	scanf("%d",&my_table);
+	choose_your_limit_message();
+	scanf("%d", &my_limit);
+	for (i = 0; i < my_limit; i++)
+	{
+		printf("%d x %d  = %d \n", my_table, i , my_table*i);
+		/* code */
+	}
+
+	return 0;
 }
 
 int main(int argc, char const *argv[])
 {
 	/* code */
+
+	welcome_message();
+	multiplication();
 	return 0;
 }
