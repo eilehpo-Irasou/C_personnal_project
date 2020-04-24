60eb8f9dd4e65a7301be72db073f1b47fe44c9b8
eilehpo-Irasou <51323756+eilehpo-Irasou@users.noreply.github.com>
2020-04-24 22:21:40 +0200

    Add files via upload

---
+++ pythagoras_theorem/main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include<math.h>
+void print_welcome_message()
+{
+	printf("THIS PROGRAM IS ABOUT PYTHAGORAS THEOREM.\n");
+
+}
+
+void what_length()
+{
+	printf("what is the length of the side ? \n");
+}
+
+void verify_number(double number)
+{
+	if (number <= 1 ||  number >=9)
+	{
+		printf("This is a number \n");
+		/* code */
+	}
+	else
+	{
+		printf("This is not a number\n");
+		//verify_number(number);
+
+	}
+
+}
+
+
+ void calculate_hypotenuse()
+ {
+ 	double side1 = 0;
+ 	double side2 = 0;
+ 	double hypotenuse = 0;
+
+ 	what_length();
+ 	scanf("%le",&side1);
+ 	what_length();
+ 	scanf("%le",&side2);
+
+ 	verify_number(side1);
+ 	verify_number(side2);
+
+ 	printf("%f, %f\n",side1,side2);
+
+ 	hypotenuse = sqrt((side1*side1)+(side2*side2));
+
+ 	printf("hypotenuse : %f\n",hypotenuse );
+
+ }
+
+
+
+
+
+int main(int argc, char const *argv[])
+{
+	print_welcome_message();
+	verify_number(55);
+	calculate_hypotenuse();
+
+	/* code */
+	return 0;
+}
